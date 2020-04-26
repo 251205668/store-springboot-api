f409c55dfab0c77a8829aa6508e750d8a7a614dd
251205668@qq.com <251205668@qq.com>
2020-04-26 23:57:29 +0800

    init project

---
+++ src/main/java/com/lin/missingyou/MissingyouApplication.java
@@ -0,0 +1,16 @@
+package com.lin.missingyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 努力中的杨先生
+ */
+@SpringBootApplication
+public class MissingyouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MissingyouApplication.class, args);
+    }
+
+}
